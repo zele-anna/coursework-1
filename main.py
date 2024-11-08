e042a9627af301e2fa5f23f8af71e89c1f61f1c1
Anna <azeleranskaya@gmail.com>
2024-11-08 18:57:38 +0300

    add main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+from src.views import main
+from src.services import simple_search
+from src.utils import read_excel
+from src.reports import spending_by_category
+
+
+date = "2024-11-08 18:45:00"
+
+main(date)
+
+transactions_list = read_excel("data/operations.xlsx")
+string_for_simple_search = "Пополнение"
+
+search_result_json = simple_search(transactions_list, string_for_simple_search)
+
+df = pd.DataFrame(transactions_list)
+category_to_find = "Переводы"
+
+data_for_report = spending_by_category(df, category_to_find)
